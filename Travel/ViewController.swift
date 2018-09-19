9172265a986efb0b114c1340060caa4eb54c847c
TaiHsin <peterlee0466@gmail.com>
2018-09-19 11:48:32 +0800

    add READ.md and gitignore

---
+++ Travel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Travel
+//
+//  Created by TaiHsinLee on 2018/9/19.
+//  Copyright © 2018年 TaiHsinLee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
